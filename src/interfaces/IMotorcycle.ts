6ab5a5997396b09be94ba5913aac9be0ac7364f2
annatrajano <annatrajano95@gmail.com>
2022-09-14 21:01:00 -0300

    feat: :fire: add req18  - create IMotorcycle interface

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+import { IVehicleSchema } from './IVehicle';
+
+const IMotorcycleSchema = z.object({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z
+    .number({
+      required_error: 'EngineCapacity is required',
+      invalid_type_error: 'EngineCapacity must be a number',
+    })
+    .lte(2500, { message: 'EngineCapacity must be 2500 or less' }),
+});
+
+const MotorcycleVehicle = z.intersection(IVehicleSchema, IMotorcycleSchema);
+
+type IMotorcycle = z.infer<typeof MotorcycleVehicle>;
+
+export { MotorcycleVehicle, IMotorcycle };
